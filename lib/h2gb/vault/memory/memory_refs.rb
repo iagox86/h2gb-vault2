e1c7509957338f04170543e454b48eede78407b9
Ron Bowes <rbowes@google.com>
2017-05-08 14:09:00 -0700

    Added a MemoryRefs class that will track references and cross-references from now on

---
+++ lib/h2gb/vault/memory/memory_refs.rb
@@ -0,0 +1,107 @@
+##
+# memory_refs.rb
+# Created May, 2017
+# By Ron Bowes
+#
+# See LICENSE.md
+#
+# A class to handle references and cross-references, because it was getting too
+# complex to handle them in-line.
+##
+
+
+require 'h2gb/vault/memory/memory_error'
+
+module H2gb
+  module Vault
+    class Memory
+      class MemoryRef
+        attr_reader :address, :refs
+        def initialize(address:, refs:)
+          if !address.is_a?(Integer)
+            raise(MemoryError, "address must be an integer!")
+          end
+          if address < 0
+            raise(MemoryError, "address must not be negative!")
+          end
+
+          if !refs.is_a?(Array)
+            raise(MemoryError, "refs must be an array!")
+          end
+          if refs.length() == 0
+            raise(MemoryError, "refs must have at least one element!")
+          end
+          refs.each do |ref|
+            if !ref.is_a?(Integer)
+              raise(MemoryError, "Each ref must be an integer!")
+            end
+          end
+
+          @address = address
+          @refs = refs.uniq().sort()
+        end
+
+        def to_s()
+          return "0x%08x => %s" % [@address, @refs.map() { |ref| "0x%04" % ref }.join(', ')]
+        end
+      end
+
+      class MemoryRefs
+        def initialize()
+          @refs = {}
+          @xrefs = {}
+        end
+
+        def insert(address:, refs:)
+          if @refs[address]
+            raise(MemoryError, "Re-declaring references on an address")
+          end
+
+          memory_ref = MemoryRef.new(address: address, refs: refs)
+          @refs[address] = memory_ref
+          refs.each do |address_ref|
+            @xrefs[address_ref] = @xrefs[address_ref] || []
+            @xrefs[address_ref] << memory_ref
+          end
+
+          return memory_ref.refs
+        end
+
+        def delete(address:)
+          memory_ref = @refs.delete(address)
+          if memory_ref.nil?
+            raise(MemoryError, "Trying to delete non-existant reference")
+          end
+
+          memory_ref.refs.each do |ref|
+            if @xrefs[ref].nil?
+              raise(MemoryError, "A cross-reference is missing!")
+            end
+            @xrefs[ref].delete(memory_ref)
+          end
+
+          return memory_ref.refs
+        end
+
+        def get_refs(address:)
+          if @refs[address].nil?
+            return []
+          end
+          return @refs[address].refs
+        end
+
+        def get_xrefs(address:)
+          xrefs = @xrefs[address] || []
+          return (xrefs.map() { |xref| xref.address }).uniq().sort()
+        end
+
+        def to_s()
+          refs  = @refs.map()  { |ref|  ref.to_s }
+          xrefs = @xrefs.map() { |xref| xref.to_s }
+
+          return "Refs:\n%s\n\nXrefs:\n%s\n" % [refs, xrefs]
+        end
+      end
+    end
+  end
+end
---
+++ test/h2gb/vault/memory/memory_refs_test.rb
@@ -0,0 +1,168 @@
+require 'test_helper'
+
+require 'h2gb/vault/memory/memory'
+require 'h2gb/vault/memory/memory_refs'
+
+class H2gb::Vault::MemoryRefsTest < Test::Unit::TestCase
+  def setup()
+    @memory_refs = H2gb::Vault::Memory::MemoryRefs.new()
+  end
+
+  def test_insert()
+    @memory_refs.insert(address: 0x0000, refs: [0x0004])
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([0x0004], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([0x0000], xrefs)
+  end
+
+  def test_insert_bad_duplicate()
+    @memory_refs.insert(address: 0x0000, refs: [0x0004])
+
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.insert(address: 0x0000, refs: [0x0008])
+    end
+  end
+
+  def get_refs_no_refs()
+    @memory_refs.insert(address: 0x0000, refs: [])
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([], refs)
+  end
+
+  def get_refs_from_not_defined_address()
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([], refs)
+  end
+
+  def test_multiple_refs()
+    # Note: These are out of order to ensure that they get sorted
+    @memory_refs.insert(address: 0x0000, refs: [0x0002, 0x0001, 0x0003])
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([0x0001, 0x0002, 0x0003], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0001)
+    assert_equal([0x0000], xrefs)
+    xrefs = @memory_refs.get_xrefs(address: 0x0002)
+    assert_equal([0x0000], xrefs)
+    xrefs = @memory_refs.get_xrefs(address: 0x0003)
+    assert_equal([0x0000], xrefs)
+  end
+
+  def test_multiple_xrefs()
+    # Note: Defining these backwards to ensure the order is sorted
+    @memory_refs.insert(address: 0x0008, refs: [0x0004])
+    @memory_refs.insert(address: 0x0000, refs: [0x0004])
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([0x0004], refs)
+
+    refs = @memory_refs.get_refs(address: 0x0008)
+    assert_equal([0x0004], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([0x0000, 0x0008], xrefs)
+  end
+
+  def get_xrefs_when_there_are_none()
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([], xrefs)
+  end
+
+  def test_duplicate_refs()
+    @memory_refs.insert(address: 0x0000, refs: [0x0004, 0x0004])
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([0x0004], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([0x0000], xrefs)
+  end
+
+  def test_self_reference()
+    @memory_refs.insert(address: 0x0000, refs: [0x0000])
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([0x0000], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0000)
+    assert_equal([0x0000], xrefs)
+  end
+
+  def test_input_validation()
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.insert(address: 'hi', refs: [0x0004])
+    end
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.insert(address: -1, refs: [0x0004])
+    end
+
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.insert(address: 0x0000, refs: 0x0004)
+    end
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.insert(address: 0x0000, refs: ['hi'])
+    end
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.insert(address: 0x0000, refs: [])
+    end
+  end
+
+  def test_delete()
+    @memory_refs.insert(address: 0x0000, refs: [0x0004])
+    @memory_refs.delete(address: 0x0000)
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([], xrefs)
+  end
+
+  def test_delete_when_there_are_multiple_refs()
+    @memory_refs.insert(address: 0x0000, refs: [0x0004, 0x0005])
+    @memory_refs.delete(address: 0x0000)
+
+    refs = @memory_refs.get_refs(address: 0x0000)
+    assert_equal([], refs)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([], xrefs)
+    xrefs = @memory_refs.get_xrefs(address: 0x0005)
+    assert_equal([], xrefs)
+  end
+
+  def test_delete_when_there_are_multiple_xrefs()
+    @memory_refs.insert(address: 0x0000, refs: [0x0004])
+    @memory_refs.insert(address: 0x0001, refs: [0x0004])
+    @memory_refs.insert(address: 0x0002, refs: [0x0004])
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([0x0000, 0x0001, 0x0002], xrefs)
+
+    @memory_refs.delete(address: 0x0000)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([0x0001, 0x0002], xrefs)
+
+    @memory_refs.delete(address: 0x0001)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([0x0002], xrefs)
+
+    @memory_refs.delete(address: 0x0002)
+
+    xrefs = @memory_refs.get_xrefs(address: 0x0004)
+    assert_equal([], xrefs)
+  end
+
+  def test_delete_missing()
+    assert_raises(H2gb::Vault::Memory::MemoryError) do
+      @memory_refs.delete(address: 0x0000)
+    end
+  end
+end

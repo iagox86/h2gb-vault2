af685479a6c068be3f1151c3dd13de747a664436
iagox86 <ron-git@skullsecurity.org>
2017-05-14 12:39:10 -0700

    Hook up the new references code to the Updater

--- lib/h2gb/vault/types/updater.rb
+++ lib/h2gb/vault/types/updater.rb
@@ -30,6 +30,13 @@ module H2gb
       end
 
       private
+      def _validate_address(address:)
+        if !address.is_a?(Integer) || address < 0
+          raise MemoryError("address must be a positive integer")
+        end
+      end
+
+      private
       def _do_item(item:)
         if item[:action].is_a?(String)
           item[:action] = item[:action].to_sym()
@@ -37,31 +44,39 @@ module H2gb
         if !item[:action].is_a?(Symbol)
           raise MemoryError("action must be a String or Symbol")
         end
-        if !item[:address].is_a?(Integer) || item[:address] < 0
-          raise MemoryError("address must be a positive integer")
-        end
 
         # Do the action
         case item[:action]
         when :define_basic_type
+          _validate_address(address: item[:address])
+
           _define_basic_type(item: item)
+
+          # Apply a comment if one exists
+          if item[:comment]
+            @memory.set_comment(address: item[:address], comment: item[:comment])
+          end
+
+          # Apply user-defined if it exists
+          if item[:user_defined]
+            @memory.replace_user_defined(address: item[:address], user_defined: item[:user_defined])
+          end
         when :set_comment
+          _validate_address(address: item[:address])
           @memory.set_comment(address: item[:address], comment: item[:comment])
-        when :add_reference
-          @memory.add_reference(address: item[:address], type: item[:type], to: item[:to])
+          _validate_address(address: item[:address])
+        when :add_refs
+          _validate_address(address: item[:address])
+          @memory.add_refs(type: item[:type], from: item[:address], tos: item[:tos])
+        when :remove_refs
+          _validate_address(address: item[:address])
+          @memory.remove_refs(type: item[:type], from: item[:address], tos: item[:tos])
+        when :undefine
+          _validate_address(address: item[:address])
+          @memory.undefine(address: item[:address], length: item[:length])
         else
           # TODO: This raise isn't working, but I plan to move MemoryError anyways
           raise H2gb::Vault::Memory::MemoryError("Unknown action: %s" % item[:action])
-        end
-
-        # Apply a comment if one exists
-        if item[:comment]
-          @memory.set_comment(address: item[:address], comment: item[:comment])
-        end
-
-        # Apply user-defined if it exists
-        if item[:user_defined]
-          @memory.replace_user_defined(address: item[:address], user_defined: item[:user_defined])
         end
       end
 
